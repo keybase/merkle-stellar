54c1a00b9e5ce47252d9afcbca2b31081871a0f4
Maxwell Krohn <themax@gmail.com>
2020-01-24 13:05:37 -0500

    small demo to read keybase root from stellar
    - from there, to traverse down to a leaf, and to play a sigchain
    - fairly modern and self-contained, but we can do futher work here
    - in particular, look to update kbpgp, especially only for the KB side (removing PGP)

---
+++ declarations.d.ts
@@ -0,0 +1,2 @@
+declare module 'kbpgp'
+declare module 'purepack'
---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+import {StellarPublicKey, Kid} from './types'
+
+export const horizonServerURI = 'https://horizon.stellar.org'
+export const keybaseStellarAddress = 'GA72FQOMHYUCNEMZN7GY6OBWQTQEXYL43WPYCY2FE3T452USNQ7KSV6E' as StellarPublicKey
+export const keybaseRootKid = '01209ec31411b9b287f62630c2486005af27548ba62a59bbc802e656b888991a20230a' as Kid
+export const keybaseAPIServerURI = 'https://keybase.io/_/api/1.0/'
