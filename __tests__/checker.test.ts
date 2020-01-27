d0a0292d5b103393d9202a3360ed24220f05ae16
Maxwell Krohn <themax@gmail.com>
2020-01-27 16:52:57 -0500

    lookups by username and fancy CLI tool
    - slightly better error handling too
    - tests are now mocked out

---
+++ __tests__/checker.test.ts
@@ -0,0 +1,43 @@
+import axios, {AxiosRequestConfig, AxiosInstance} from 'axios'
+jest.mock('axios')
+import {apiMock} from './api_mock'
+
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+// The Stellar toolkit uses a static Axios object with interceptors,
+// so we need to mock out more of the object than usual.
+mockedAxios.create.mockImplementation(
+  (): AxiosInstance => {
+    return {
+      interceptors: {
+        response: {
+          use: (): void => {},
+        },
+      },
+      get: async (url: string): Promise<unknown> => {
+        return apiMock(url)
+      },
+    } as AxiosInstance
+  }
+)
+
+mockedAxios.get.mockImplementation(
+  (url: string, config?: AxiosRequestConfig): Promise<unknown> => {
+    return apiMock(url)
+  }
+)
+
+import {Uid} from '../lib/types'
+import {Checker} from '../lib'
+
+test('Can fetch user max from the tree by UID', async (): Promise<void> => {
+  const chk = new Checker()
+  const ret = await chk.checkUid('dbb165b7879fe7b1174df73bed0b9500' as Uid)
+  expect(ret.length).toBeGreaterThanOrEqual(691)
+})
+
+test('Can fetch user max from the tree by username', async (): Promise<void> => {
+  const chk = new Checker()
+  const ret = await chk.checkUsername('max' as Uid)
+  expect(ret.length).toBeGreaterThanOrEqual(691)
+})
---
+++ src/reporter.ts
@@ -0,0 +1,99 @@
+import ora from 'ora'
+
+export interface Step {
+  start(s?: string): void
+  update(s: string): void
+  success(s: string): void
+  fail(s: string): void
+  step(): Step
+}
+export interface Reporter {
+  step(s: string): Step
+  error(e: Error): void
+}
+
+export class NullStep {
+  constructor(s: string) {
+    return
+  }
+  start(s?: string): void {
+    return
+  }
+  update(s: string): void {
+    return
+  }
+  success(s: string): void {
+    return
+  }
+  fail(s: string): void {
+    return
+  }
+  step(): Step {
+    return new NullStep('null')
+  }
+}
+
+export class NullReporter implements Reporter {
+  step(s: string): Step {
+    return new NullStep(s)
+  }
+  error(e: Error): void {
+    return
+  }
+}
+
+class InteractiveStep implements Step {
+  prefix: string
+  spinner: ora.Ora
+  constructor(p: string) {
+    this.prefix = p
+    this.spinner = ora(p)
+    return
+  }
+
+  fmt(s: string): string {
+    return [this.prefix, s].join(': ')
+  }
+  start(s?: string): void {
+    if (!s) {
+      this.spinner.start()
+      return
+    }
+    this.spinner.start(this.fmt(s))
+    return
+  }
+  update(s: string): void {
+    this.spinner.text = this.fmt(s)
+    return
+  }
+  success(s: string): void {
+    this.spinner.succeed(this.fmt(s))
+    return
+  }
+  fail(s: string): void {
+    this.spinner.fail(this.fmt(s))
+    return
+  }
+  step(): Step {
+    return new NullStep('null')
+  }
+}
+
+export class InteractiveReporter implements Reporter {
+  n: number
+  last: InteractiveStep | null
+  constructor() {
+    this.n = 0
+  }
+  step(s: string): InteractiveStep {
+    this.n++
+    const ret = new InteractiveStep(`${this.n}. ${s}`)
+    this.last = ret
+    return ret
+  }
+  error(e: Error): void {
+    if (this.last != null) {
+      this.last.fail(e.toString())
+    }
+  }
+}
