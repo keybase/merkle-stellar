b28abf2db074c6304a5ad69ccc2f71850b350ff0
Maxwell Krohn <themax@gmail.com>
2020-02-09 21:11:36 -0500

    more test

--- __tests__/checker.test.ts
+++ __tests__/checker.test.ts
@@ -34,14 +34,14 @@ import {Checker} from '../lib'
 test('Can fetch user max from the tree by UID', async (): Promise<void> => {
   const chk = new Checker()
   const ret = await chk.checkUid('dbb165b7879fe7b1174df73bed0b9500' as Uid)
-  expect(ret.links.length).toBeGreaterThanOrEqual(691)
+  expect(ret.links.length).toBe(691)
   expect(ret.resets).toBeNull()
 })
 
 test('Can fetch user max from the tree by username', async (): Promise<void> => {
   const chk = new Checker()
   const ret = await chk.checkUsername('max')
-  expect(ret.links.length).toBeGreaterThanOrEqual(691)
+  expect(ret.links.length).toBe(691)
   expect(ret.resets).toBeNull()
 })
 
@@ -57,4 +57,13 @@ test('Can replay a long reset chain', async (): Promise<void> => {
   const ret = await chk.checkUid('10286214005a3b5c1c284b7374e97c19' as Uid)
   expect(ret.links.length).toBe(138)
   expect(ret.resets.length).toBe(8)
+})
+
+// Here the stellar root is about a week behind the live root, and there were
+// ~6 sigs in between. Test can use some more assertions.
+test('Can replay skips when loading mikem', async (): Promise<void> => {
+  const chk = new Checker()
+  const ret = await chk.checkUsername('mikem')
+  expect(ret.links.length).toBe(574)
+  expect(ret.resets).toBeNull()
 })
