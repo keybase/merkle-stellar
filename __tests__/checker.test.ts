9f50da23fff3085941930d1f1c8606c8194050e9
Maxwell Krohn <themax@gmail.com>
2021-04-13 13:24:14 -0400

    unmock axios to just get it to work

--- __tests__/checker.test.ts
+++ __tests__/checker.test.ts
@@ -1,33 +1,3 @@
-import axios, {AxiosRequestConfig, AxiosInstance} from 'axios'
-
-jest.mock('axios')
-import {apiMock} from './api_mock'
-
-const mockedAxios = axios as jest.Mocked<typeof axios>
-
-// The Stellar toolkit uses a static Axios object with interceptors,
-// so we need to mock out more of the object than usual.
-mockedAxios.create.mockImplementation(
-  (): AxiosInstance => {
-    return {
-      interceptors: {
-        response: {
-          use: (): void => {},
-        },
-      },
-      get: async (url: string): Promise<unknown> => {
-        return apiMock(url)
-      },
-    } as AxiosInstance
-  }
-)
-
-mockedAxios.get.mockImplementation(
-  (url: string, config?: AxiosRequestConfig): Promise<unknown> => {
-    return apiMock(url)
-  }
-)
-
 import {Uid} from '../lib/types'
 import {Checker} from '../lib'
 
